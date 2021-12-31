46e6f3979ecbcbf7a33ccd4f9552dd8a29130b9f
Mônica S <monica.s.bergues@gmail.com>
2021-12-30 23:58:15 -0300

    feat: added login page and recovery password

---
+++ src/components/sideMenu/style.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 200px;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: stretch;
+  a {
+    color: #3a3a3a;
+    padding-left: 20px;
+    cursor: pointer;
+  }
+  img {
+    margin-top: 10px;
+    width: 100px;
+  }
+  div {
+    width: 90%;
+    height: 5%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin-top: 10px;
+    .icon {
+      width: 15px;
+      height: 15px;
+      margin: 0;
+    }
+  }
+  .Logout {
+    width: 100%;
+    font-size: 1rem;
+    button {
+      margin-left: 20px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      background: none;
+      border: none;
+    }
+    position: absolute;
+    bottom: 20px;
+    cursor: pointer;
+  }
+  position: absolute;
+  background-color: white;
+`;
